cfa8fe1a1c4f69f2d55a4e47fa0c92b0e1d5c592
Ian Binnie <support@binnie.id.au>
2021-02-24 11:45:16 +1100

    Fix directory structure
    Fix corrupted directory structure

--- source/cpuinfo.h
+++ source/cpuinfo.h
@@ -24,7 +24,7 @@ SOFTWARE.
 #define CPUINFO_H
 typedef struct
 {
-   int p1_revision;
+   int p1_revision;	// P1 Header 0:None, 1:Pi B, 2:Pi B V2, 3:40 pin
    char *ram;
    char *manufacturer;
    char *processor;
